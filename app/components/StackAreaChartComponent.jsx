7bdc82e05d26c912a34c0bcddd49e4a558ef30ea
Do Chi Thanh <chithanhdo2002@gmail.com>
2023-11-12 13:25:09 +0700

    Stack area chart component created

---
+++ app/components/StackAreaChartComponent.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Card, SkeletonBodyText } from "@shopify/polaris";
+import { PolarisVizProvider, StackedAreaChart } from "@shopify/polaris-viz";
+import "@shopify/polaris/build/esm/styles.css";
+import { ClientOnly } from "remix-utils/client-only";
+
+export default function LineChartComponent({ data }) {
+  return (
+    <ClientOnly
+      fallback={
+        <Card>
+          <SkeletonBodyText />
+        </Card>
+      }
+    >
+      {() => {
+        return (
+          <PolarisVizProvider
+            themes={{
+              Default: {
+                chartContainer: {
+                  minHeight: 200,
+                  sparkChartMinHeight: 200,
+                  borderRadius: "8px",
+                  padding: "55px 15px 15px 15px",
+                },
+              },
+            }}
+          >
+            <div style={{ position: "relative" }}>
+              <p
+                style={{
+                  position: "absolute",
+                  top: 12,
+                  left: 12,
+                  zIndex: 1,
+                }}
+              >
+                2020-2023
+              </p>
+              <LineChart data={data} />
+            </div>
+          </PolarisVizProvider>
+        );
+      }}
+    </ClientOnly>
+  );
+}
